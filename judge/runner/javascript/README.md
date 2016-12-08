ab59aef096069b669af29713841cceddae2b8cfa
Zhang Wenqing <winking.zhang@grapecity.com>
2016-12-08 16:50:48 +0800

    refact nodejs running

--- judge/runner/javascript/README.md
+++ judge/runner/javascript/README.md
@@ -1,1 +1,28 @@
 the judge runner for node.js
+
+# Sandbox
+build on [sandbox](https://www.npmjs.com/package/sandbox), for more information, see [https://github.com/gf3/sandbox](https://github.com/gf3/sandbox)
+
+# Process model
+```
+##############################
+#      current process       #
+#########|########^###########
+         |        ^
+ command | (pipe) | message
+         v        |
+*********v********|***********
+* child process (Sandbox VM) *
+******************************
+```
+
+# Project structure
+```
+|- index.js         * the main logic, will bootstap a Sandbox to run user code
++- data
+  |- cases          * all use cases to be running
+    |- 1.in         * input file for case 1
+    |- 1.out        * expect output for case 1
+  |- source.js      * the source code of solution
+  |- result.json    * the result (score and outputs)
+```
--- judge/runner/javascript/index.js
+++ judge/runner/javascript/index.js
@@ -1,35 +1,107 @@
 var fs = require('fs'),
+    path = require('path'),
     util = require('util'),
     Sandbox = require("sandbox");
 
 // With following assume, [data] folder will place all data files
 // +- data
-//   |- cases.json     * all use cases to be running
+//   |- cases          * all use cases to be running
+//     |- 1.in         * input file for case 1
+//     |- 1.out        * expect output for case 1
 //   |- source.js      * the source code of solution
 //   |- result.json    * the result (score and outputs)
 
-fs.readFile('data/cases.json', 'utf8', function (err1, casesText) {
-    if (err1) {
-        process.stderr.write('Error read file [data/cases.json]: ' + err1);
-        return;
-    }
-    var cases = JSON.parse(casesText);
-    if (cases.length > 0) {
-        fs.readFile('data/source.js', 'utf8', function (err2, code) {
-            if (err2) {
-                process.stderr.write('Error read file [data/code.js]: ' + err2);
-                return;
-            }
+var defaultTimeout = 2000,
+    datadir = 'data',
+    casedir = datadir + '/cases',
+    caseInPostfix = '.in',
+    caseOutPostfix = '.out',
+    currentRunIndex = 0,
+    result = {score: 0, outputs: []};
 
-            for (var i in cases) {
-                var box = new Sandbox({timeout: 2000});
-                box.cs = cases[i];
-                box.run(code + '; process("' + cases[i].input + '");',
-                    function (output) {
-                        var cs = this.cs;
-                        console.log('' + cs.id + ':' + util.inspect(cs.expect) + '<>' + output.result);
-                    });
-            }
-        });
+try {
+    // clean old result
+    fs.writeFileSync(datadir + '/result.json', JSON.stringify(result), 'utf8');
+
+    var source = fs.readFileSync(datadir + '/source.js', 'utf8'),
+        cases = (function (files) {
+            return files.map(function (file) {
+                return path.join(casedir, file);
+            }).filter(function (file) {
+                return path.extname(file) === caseInPostfix && fs.statSync(file).isFile();
+            }).map(function (file) {
+                var caseId = path.basename(file, caseInPostfix);
+                return {
+                    id: parseInt(caseId, 10),
+                    input: readFileContent(file),
+                    expect: readExpectFile(caseId)
+                };
+            }).sort(function (cs1, cs2) {
+                return cs1.id - cs2.id;
+            });
+        })(fs.readdirSync(casedir));
+
+    for (var i = 0; i < cases.length; i++) {
+        runCase(i, cases[i]);
     }
-});
+
+} catch (any) {
+    process.stderr.write('Run case error: ' + any);
+    return;
+}
+
+function runCase(index, cs) {
+    var caseId = cs.id;
+    if (index > currentRunIndex) {
+        setTimeout(function () {
+            // next tick, run again
+            runCase(index, cs);
+        }, (index - currentRunIndex) * 10);
+    } else {
+        // yep, resource ready, run the case
+        var box = new Sandbox({timeout: defaultTimeout});
+        box.run(source + '; process("' + cs.input + '");',
+            function (output) {
+                var expected = util.inspect(cs.expect),
+                    actual = output.result;
+                if (expected !== actual) {
+                    expected = deepTrim(expected);
+                    actual = deepTrim(actual);
+                }
+                if (expected === actual) {
+                    result.score += Math.pow(cs.id, 2);
+                }
+                result.outputs[caseId - 1] = output;
+
+                // done, move to next case
+                currentRunIndex++;
+
+                if (currentRunIndex === cases.length) { // run last one
+                    fs.writeFileSync(datadir + '/result.json', JSON.stringify(result), 'utf8');
+                }
+            });
+    }
+}
+
+function readFileContent(file) {
+    return fs.readFileSync(file, 'utf8');
+}
+
+function readExpectFile(caseId) {
+    return readFileContent(path.join(casedir, caseId + caseOutPostfix));
+}
+
+function deepTrim(realString) {
+    realString = realString.trim();
+    var resultString = '', lastChar = '';
+    for (var i = 0; i < realString.length; i++) {
+        var currentChar = realString[i];
+        if ((lastChar === ' ' || lastChar === '\u00A0') &&
+            (currentChar === ' ' || currentChar === '\u00A0')) {
+            continue;
+        }
+        resultString += currentChar;
+        lastChar = currentChar;
+    }
+    return resultString;
+}
