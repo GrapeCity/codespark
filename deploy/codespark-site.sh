fd4bb61916f4899f3b47a7218df22c1307b6ece2
Zhang Wenqing <winking.zhang@grapecity.com>
2016-12-15 15:21:54 +0800

    update deployment scripts and config - some typo

--- deploy/codespark-site.sh
+++ deploy/codespark-site.sh
@@ -3,8 +3,8 @@
 docker run -d \
     --name site \
     -p 80:80 \
-    --link service1:codespark-service-1 \
-    --link service2:codespark-service-2 \
+    --link service1:service1 \
+    --link service2:service2 \
     -v $(pwd)/conf/site/nginx.conf:/etc/nginx/nginx.conf \
     -v $(pwd)/conf/site/conf.d:/etc/nginx/conf.d \
     codespark-site:1.0-$(date +"%Y%m%d")
