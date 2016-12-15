afe454be81776fee5c9136b7771f52390930df0d
Zhang Wenqing <winking.zhang@grapecity.com>
2016-12-15 21:25:54 +0800

    update deploy scripts

--- deploy/drop-server.sh
+++ deploy/drop-server.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
 
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
+docker rm -f management
+docker rm -f site
+docker rm -f service1
+docker rm -f service2
+docker rm -f redis
+docker rm -f mongo
+
+BUILD_VERSION=$2
+if [ -z "$BUILD_VERSION" ]
+then
+    BUILD_VERSION=$(date +"%Y%m%d")
+fi
+
+DROP_IMAGES=$1
+if [ ! -z "DROP_IMAGES" ]
+then
+    docker rmi codespark-management:1.0-$BUILD_VERSION
+    docker rmi codespark-service:1.0-$BUILD_VERSION
+    docker rmi codespark-site:1.0-$BUILD_VERSION
+fi
