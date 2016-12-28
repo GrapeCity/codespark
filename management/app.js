6474dc0346c31b8d1a3546cfcaa22859238cd745
Zhang Wenqing <winking.zhang@grapecity.com>
2016-12-28 13:44:48 +0800

    increase backend management service's post restricts

--- management/app.js
+++ management/app.js
@@ -51,8 +51,8 @@ app.use('/fonts', express.static(path.join(__dirname, 'public/fonts')));
 app.use('/img', express.static(path.join(__dirname, 'public/img')));
 
 // Request body parsing middleware should be above methodOverride
-app.use(bodyParser.json({limit: '10mb'}));
-app.use(bodyParser.urlencoded({limit: '10mb', extended: true}));
+app.use(bodyParser.json({limit: '20mb'}));
+app.use(bodyParser.urlencoded({limit: '20mb', extended: true}));
 
 // Add the cookie parser and flash middleware
 app.use(cookieParser());
