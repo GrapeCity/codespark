d7e0941960647d2af63d21b7c6763db50014c393
Zhang Wenqing <winking.zhang@grapecity.com>
2017-01-04 11:57:03 +0800

    imp react ui

--- site/src/js/components/topMenu.js
+++ site/src/js/components/topMenu.js
@@ -1,9 +1,65 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 export default class TopMenu extends React.Component {
     render() {
         return (
-            <div>TopMenu</div>
+            <nav className="navbar navbar-transparent navbar-fixed-top navbar-color-on-scroll">
+                <div className="container">
+                    <div className="navbar-header">
+                        <button type="button" className="navbar-toggle" data-toggle="collapse"
+                                data-target="#navigation-index">
+                            <span className="sr-only">Toggle navigation</span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                        </button>
+                        <a href="#">
+                            <div className="logo-container">
+                                <div className="logo">
+                                    <img src="img/logo.png" alt="GrapeCity Logo"/>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div className="collapse navbar-collapse" id="navigation-index">
+                        <ul className="nav navbar-nav navbar-right">
+                            <li>
+                                <Link to="/contests" className="right-separator">
+                                    编程挑战赛
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/rules" className="right-separator">
+                                    规则和注意事项
+                                </Link>
+                            </li>
+                            <li>
+                                <a rel="tooltip" title="<img src='/img/grapecityxianqr.png' alt='微信'>"
+                                   data-placement="bottom" data-html="true" target="_blank"
+                                   className="btn btn-white btn-simple btn-just-icon">
+                                    <i className="fa fa-wechat"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a rel="tooltip" title="关注葡萄城官方微博" data-placement="bottom"
+                                   href="http://www.weibo.com/powertools" target="_blank"
+                                   className="btn btn-white btn-simple btn-just-icon">
+                                    <i className="fa fa-weibo"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a rel="tooltip" title="葡萄城控件技术团队博客" data-placement="bottom"
+                                   href="http://powertoolsteam.cnblogs.com/" target="_blank"
+                                   className="btn btn-white btn-simple btn-just-icon">
+                                    <i className="fa fa-wordpress"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         );
     }
 }
--- site/src/js/index.js
+++ site/src/js/index.js
@@ -1,9 +1,92 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {createStore, combineReducers} from 'redux';
+import {Provider} from 'react-redux';
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+import {syncHistoryWithStore, routerReducer} from 'react-router-redux'
+
+import Home from './containers/home';
+
+import * as stores from './stores';
+const reducer = combineReducers(stores);
+const store = createStore(
+    combineReducers({
+        ...reducer,
+        routing: routerReducer
+    }));
+
+// Create an enhanced history that syncs navigation events with the store
+const history = syncHistoryWithStore(browserHistory, store);
+
+class Root extends React.Component {
+    render() {
+        return (
+            <div>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+const NoMatch = React.createClass({
+    render() {
+        return <h3>Route is not found</h3>
+    }
+});
+
+const Login = React.createClass({
+    render() {
+        return <h3>Login</h3>
+    }
+});
+const Signup = React.createClass({
+    render() {
+        return <h3>Signup</h3>
+    }
+});
+const User = React.createClass({
+    render() {
+        return <h3>User</h3>
+    }
+});
+const Rules = React.createClass({
+    render() {
+        return <h3>Rules</h3>
+    }
+});
+const Contests = React.createClass({
+    render() {
+        return <h3>Contests</h3>
+    }
+});
+const Contest = React.createClass({
+    render() {
+        return <h3>Contest</h3>
+    }
+});
+
+const pageLoaded = (nextState, transition) => {
+    // make sure this function return AS SOON AS POSSIBLE
+    setTimeout(() => {
+        $ && $.loading && $.loading.hide && $.loading.hide();
+    }, 500);
+};
 
 render(
-    <div>
-        This is sample content from REACT.js
-    </div>,
+    (<Provider store={store}>
+        <Router history={history}>
+            <Route path="/" component={Root} onEnter={pageLoaded}>
+                <IndexRoute component={Home}/>
+                <Route path="login" component={Login}/>
+                <Route path="signup" component={Signup}/>
+                <Route path="me" component={User}/>
+                <Route path="rules" component={Rules}/>
+                <Route path="contests" component={Contests}>
+                    <Route path="/contest/:contestName" component={Contest}/>
+                </Route>
+            </Route>
+            <Route path="*" component={NoMatch}/>
+        </Router>
+    </Provider>),
     document.getElementById('root')
 );
