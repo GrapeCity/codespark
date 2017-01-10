443f9de4407d4a5c2c438627575bb4b3a91c46e2
Zhang Wenqing <winking.zhang@grapecity.com>
2017-01-10 10:40:10 +0800

    fix join contest

--- service/repositories/contestRepository.js
+++ service/repositories/contestRepository.js
@@ -33,8 +33,8 @@ class ContestRepository extends CacheableRepository {
                 user   : user._id,
                 contest: contest._id,
                 score  : 0,
-                begin  : contest.begin,
-                end    : contest.end
+                begin  : moment(contest.begin, 'LLL').toDate(),
+                end    : moment(contest.end, 'LLL').toDate()
             });
             obj.save(err => {
                 if (err) {
@@ -42,7 +42,7 @@ class ContestRepository extends CacheableRepository {
                 }
                 let real     = obj.toObject();
                 real.user    = user;
-                real.contest = contest.toObject();
+                real.contest = contest;
                 resolve(real);
             });
         });
--- service/routers/contests.js
+++ service/routers/contests.js
@@ -1,11 +1,11 @@
-let express = require('express'),
-    utils = require('../utils'),
-    logger = utils.winston.appLogger,
-    router = express.Router(),
+let express           = require('express'),
+    utils             = require('../utils'),
+    logger            = utils.winston.appLogger,
+    router            = express.Router(),
     ContestRepository = require('../repositories/contestRepository');
 
 router.get('/:name', (req, res, next) => {
-    let name = req.params.name,
+    let name        = req.params.name,
         contestRepo = new ContestRepository();
 
     contestRepo.findByName(name)
@@ -15,10 +15,10 @@ router.get('/:name', (req, res, next) => {
                 contestRepo.getTop10(contest._id)
             ]).then(data => {
                 res.locals.validation = [];
-                res.locals.form = {
-                    board: data[0],
+                res.locals.form       = {
+                    board  : data[0],
                     contest: contest,
-                    top10: data[1]
+                    top10  : data[1]
                 };
                 return res.render('contest/index');
             }).catch(err => {
@@ -30,6 +30,58 @@ router.get('/:name', (req, res, next) => {
             });
         })
         .catch(err => {
+            res.locals.validation = [`竞赛（${name}）不存在`];
+            return res.render('contest/error');
+        });
+});
+
+router.post('/:name', (req, res, next) => {
+    let name        = req.params.name,
+        contestRepo = new ContestRepository();
+
+    contestRepo.findByName(name)
+        .then(contest => {
+            contestRepo.findOneByIdAndUser(contest._id, req.user._id)
+                .then(uc => {
+                    contestRepo.getTop10(contest._id)
+                        .then(top10 => {
+                            res.locals.validation = [];
+                            res.locals.form       = {
+                                board  : uc,
+                                contest: contest,
+                                top10  : top10
+                            };
+                            return res.render('contest/index');
+                        })
+                        .catch(err => {
+                            next(err);
+                        });
+                })
+                .catch(err => {
+                    if (err.status !== 404) {
+                        return next(err);
+                    }
+                    Promise.all([
+                        contestRepo.createUserContest(req.user, contest),
+                        contestRepo.getTop10(contest._id)
+                    ]).then(data => {
+                        res.locals.validation = [];
+                        res.locals.form       = {
+                            board  : data[0],
+                            contest: contest,
+                            top10  : data[1]
+                        };
+                        return res.render('contest/index');
+                    }).catch(err => {
+                        if (err.status === 404) {
+                            res.locals.validation = [`竞赛（${name}）不存在，或者你没有加入该竞赛，请从<a href="/dashboard">dashboard</a>页面重试`];
+                            return res.render('contest/error');
+                        }
+                        next(err);
+                    });
+                });
+        })
+        .catch(er => {
             res.locals.validation = [`竞赛（${name}）不存在`];
             return res.render('contest/error');
         });
