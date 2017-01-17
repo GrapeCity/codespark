22205e34056ab1bbe417f868944e316b3ea32df2
Zhang Wenqing <winking.zhang@grapecity.com>
2017-01-17 08:31:45 +0800

    fix judge comparison

--- judge/runner/javascript/index.js
+++ judge/runner/javascript/index.js
@@ -36,28 +36,45 @@ try {
             .sort((cs1, cs2) => cs1.id - cs2.id))(fs.readdirSync(casedir));
 
     runCaseAsync(cases, source, () => {
-        process.stdout.write(`calc final score, write result to file`);
+        process.stdout.write(`calc final score, write result to file\n`);
         final.score = final.count * final.count;
         fs.writeFileSync(datadir + '/result.json', JSON.stringify(final), 'utf8');
     });
 } catch (any) {
-    process.stderr.write('Internal error: ' + any);
+    process.stderr.write(`Internal error: ${any} \n`);
     return;
 }
 
 function runCaseAsync(cases, source, next) {
     let cs   = cases.shift(),
         csId = cs.id;
-    process.stdout.write(`run case [id: ${csId}], left ${cases.length} in pending`);
+    process.stdout.write(`run case [id: ${csId}], left ${cases.length} in pending\n`);
     new Sandbox({timeout: defaultTimeout})
         .run(source + '; process("' + cs.input + '");',
             output => {
                 output.id    = csId;
-                let expected = util.inspect(cs.expect),
+                let expected = cs.expect,
                     actual   = output.result;
+                process.stdout.write(`comparing expected and actual for case [id: ${csId}]: \n`);
+                process.stdout.write(`expected=${expected}\n`);
+                process.stdout.write(`actual  =${actual}\n`);
+                // first compare all original output
                 if (expected !== actual) {
+                    // second deep trim all blank and newline
                     expected = deepTrim(expected);
                     actual   = deepTrim(actual);
+
+                    if (expected !== actual) {
+                        // third wrap data if result is string
+                        expected = util.inspect(cs.expect);
+                        actual   = output.result;
+
+                        if (expected !== actual) {
+                            // fourth deep trim again
+                            expected = deepTrim(expected);
+                            actual   = deepTrim(actual);
+                        }
+                    }
                 }
                 if (expected === actual) {
                     output.passed = true;
