72022a6b8cf0820266bcb1d8f4dcfc46b160ceb6
Zhang Wenqing <winking.zhang@grapecity.com>
2017-01-18 11:35:39 +0800

    fix judge agent syntax error

--- judge/runner/javascript/index.js
+++ judge/runner/javascript/index.js
@@ -50,7 +50,7 @@ function runCaseAsync(cases, source, next) {
         csId = cs.id;
     process.stdout.write(`run case [id: ${csId}], left ${cases.length} in pending\n`);
     new Sandbox({timeout: defaultTimeout})
-        .run(source + '; __proc__("' + cs.input + '");',
+        .run(source + '; __proc__("' + cs.input.replace(/\n/g,'\\n') + '");',
             output => {
                 output.id    = csId;
                 let expected = cs.expect,
